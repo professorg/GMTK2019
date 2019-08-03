57a2982374047404aa3680876ee73dd31cb1c0c6
Grant VanDomelen <grant.vandomelen@gmail.com>
2019-08-03 13:27:58 -0700

    Moving Platforms are a go

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
 
-    public List<Vector2> points;
+    public Vector2[] movements;
+    public float speed;
+    bool reverse;
+    int index;
+    float remaining;
+    Vector3 nextPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        reverse = false;
+        index = 0;
+        remaining = movements[0].magnitude;
+        nextPos = transform.position + (Vector3)movements[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed*Time.deltaTime);
+        if ((transform.position - nextPos).sqrMagnitude < 0.001) {
+            if (reverse) {
+                if (index == 0) { // Back to start: reverse
+                    reverse = false;
+                } else {
+                    index--; // Go to next movement in reverse
+                }
+            } else {
+                if (index == movements.Length - 1) { // At end: reverse
+                    reverse = true;
+                } else {
+                    index++; // Go to next movement
+                }
+            }
+            nextPos = nextPos + (reverse ? -1 : 1) * (Vector3)movements[index];
+        }
     }
+
 }
---
+++ Assets/Scripts/StickBox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickBox : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider) {
+        Debug.Log("Squish?");
+        if (collider.gameObject.tag == "Player") {
+            Debug.Log("Squish!");
+            collider.gameObject.GetComponent<Transform>().parent = transform;
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Player") {
+            collider.gameObject.GetComponent<Transform>().parent = null;
+        }
+    }
+}
