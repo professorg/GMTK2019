6205fb7915133c2516715e3c37e8e92beb822bff
Grant VanDomelen <grant.vandomelen@gmail.com>
2019-08-03 12:03:39 -0700

    Added platform sprites

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public List<Vector2> points;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
