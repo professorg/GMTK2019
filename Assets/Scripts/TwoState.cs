3f02553db4ab0fabfb4331231943e3590d436f56
Grant VanDomelen <grant.vandomelen@gmail.com>
2019-08-03 16:28:53 -0700

    Added levers.

---
+++ Assets/Scripts/TwoState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoState : MonoBehaviour
+{
+
+    public bool facingLeft;
+    public float resetTime;
+    public Sprite spriteLeft, spriteMiddle, spriteRight;
+
+    float timeLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = 0;
+        GetComponent<SpriteRenderer>().sprite = (facingLeft ? spriteLeft : spriteRight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0) {
+            timeLeft -= Time.deltaTime;
+        } else {
+            GetComponent<SpriteRenderer>().sprite = (facingLeft ? spriteLeft : spriteRight);
+        }
+    }
+
+    public void Notify(bool left) {
+        if (left == facingLeft) {
+            timeLeft = resetTime;
+            GetComponent<SpriteRenderer>().sprite = spriteMiddle;
+        } else if (timeLeft > 0) {
+            facingLeft = !facingLeft;
+            timeLeft = resetTime;
+            GetComponent<SpriteRenderer>().sprite = (facingLeft ? spriteLeft : spriteRight);
+        }
+    }
+}
---
+++ Assets/Scripts/TwoStateTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoStateTrigger : MonoBehaviour
+{
+
+    public bool left;
+    public TwoState parent;
+
+    void OnTriggerExit2D(Collider2D collider) {
+        parent.Notify(left);
+    }
+
+}
