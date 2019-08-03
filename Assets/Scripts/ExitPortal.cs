847dd78313e0a28d88b9668c1ee08a419d8ca86a
Grant VanDomelen <grant.vandomelen@gmail.com>
2019-08-03 10:51:50 -0700

    Added Exit Portal with Prefab

---
+++ Assets/Scripts/ExitPortal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitPortal : MonoBehaviour
+{
+
+    public string nextLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+
+}
