3f0afae01281a1479a2220716fcadc8428cc9a82
Grant VanDomelen <grant.vandomelen@gmail.com>
2019-08-03 20:50:33 -0700

    Teleporters exist now

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+
+    public Teleporter other;
+    public float teleportCooldown;
+    float cooldown;
+    
+    void Start()
+    {
+        cooldown = 0;
+    }
+
+    void Update()
+    {
+        if (cooldown > 0) {
+            cooldown -= Time.deltaTime;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (cooldown <= 0 && collider.gameObject.tag == "Player") {
+            //Vector3 offset = transform.position - collider.transform.position;
+            //other.Cooldown();
+            //collider.transform.position = other.transform.position - offset;
+            other.Cooldown();
+            collider.transform.position = other.transform.position;
+        }
+    }
+    
+    public void Cooldown()
+    {
+        cooldown = teleportCooldown;
+    }
+
+}
