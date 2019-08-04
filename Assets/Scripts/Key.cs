6e702c2e76ed5bc1d74ac812d8595ce7b7cc7204
Grant VanDomelen <grant.vandomelen@gmail.com>
2019-08-03 21:31:43 -0700

    Doors

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+
+    public KeyDoor theDoor;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player") {
+            theDoor.Open();
+        }
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+    }
+
+}
---
+++ Assets/Scripts/KeyDoor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDoor : MonoBehaviour
+{
+
+    public void Open()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+    }
+
+}
