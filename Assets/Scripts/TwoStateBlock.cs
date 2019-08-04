59701a9449bfb79258f3cbc36a4657dbf2f85e33
Grant VanDomelen <grant.vandomelen@gmail.com>
2019-08-03 20:05:12 -0700

    Now with two state blocks!

---
+++ Assets/Scripts/TwoStateBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoStateBlock : MonoBehaviour
+{
+    public bool startEnabled;
+    public Sprite onSprite, offSprite;
+
+    bool isOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOn = startEnabled;
+        GetComponent<SpriteRenderer>().sprite = (isOn ? onSprite : offSprite);
+        GetComponent<Collider2D>().enabled = isOn;
+    }
+
+    public void Toggle()
+    {
+        isOn = !isOn;
+        GetComponent<SpriteRenderer>().sprite = (isOn ? onSprite : offSprite);
+        GetComponent<Collider2D>().enabled = isOn;
+    }
+
+}
